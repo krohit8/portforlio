fd3944d71aa2aac2c1726112301c6190bb56f6e6
krohit8 <kumarrairohit98@gmail.com>
2025-05-09 11:35:58 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@
 
 export default function Home() {
   return <div className="flex justify-center items-center h-screen text-6xl">
-    Kyu nahi ho rahi hai Parhai   ! 
+    Aaaj Mera OS exam hai   !
+<div className="text-red-900">
+
+    All the best nahi bologe? 
+</div>
   </div>
    
 
